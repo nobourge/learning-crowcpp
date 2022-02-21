25a28e8183e5728d30624fc71bd0b0000cb1b575
noe <noe.bourgeois@ulb.be>
2022-02-21 19:30:43 +0100

    /snap/clion/180/bin/cmake/linux/bin/cmake --build /home/noe/CLionProjects/crowcpp/cmake-build-debug --target crow [2/2] Linking CXX executable crow FAILED: crow

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by noe on 21.02.22.
+//
+#include "main.h"
+
+//#include "crow.h"
+#include "crow_all.h"
+
+int main()
+{
+  crow::SimpleApp app; //define your crow application
+
+  //define your endpoint at the root directory
+  CROW_ROUTE(app, "/")([](){
+    return "Hello world";
+  });
+
+  //set the port, set the app to run on multiple threads, and run the app
+  app.port(18080).multithreaded().run();
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by noe on 21.02.22.
+//
+
+#ifndef CROWCPP__MAIN_H_
+#define CROWCPP__MAIN_H_
+
+#endif //CROWCPP__MAIN_H_
